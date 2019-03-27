3519272174a2c7b7fc1864d8303ae9c3c407efce
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-27 12:25:53 +0000

    Deploy byrne-greg/testcafe-cheat-sheet to github.com/byrne-greg/testcafe-cheat-sheet.git:gh-pages

---
+++ precache-manifest.7d1416ccb66a6da733d167e3785b7f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa48354440120237a125",
+    "url": "/static/css/main.9c326860.chunk.css"
+  },
+  {
+    "revision": "fa48354440120237a125",
+    "url": "/static/js/main.686eab43.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3cc694e97c590b9df3a2",
+    "url": "/static/css/2.6113102c.chunk.css"
+  },
+  {
+    "revision": "3cc694e97c590b9df3a2",
+    "url": "/static/js/2.fedad35f.chunk.js"
+  },
+  {
+    "revision": "2bfe2608f539845726a52bb205d44939",
+    "url": "/index.html"
+  }
+];
