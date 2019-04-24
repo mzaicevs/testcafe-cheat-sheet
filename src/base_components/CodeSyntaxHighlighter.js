7fcee3c3bcbc4d797a5e720fb05fa4a53c8e5938
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2019-04-24 10:08:42 +0100

    adding vscode highlight style to code examples

--- src/base_components/CodeSyntaxHighlighter.js
+++ src/base_components/CodeSyntaxHighlighter.js
@@ -1,14 +1,11 @@
 import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
-// import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { vs2015 } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const CodeSyntaxHighlighter = props => {
   const { children, language } = props;
   return (
-    <SyntaxHighlighter
-      language={language}
-      // style={docco}
-    >
+    <SyntaxHighlighter language={language} style={vs2015}>
       {children}
     </SyntaxHighlighter>
   );
