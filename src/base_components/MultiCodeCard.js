db1f70b1310df80dc88e57c34806d0cbf6c7b430
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2019-04-24 09:13:19 +0100

    adding dividers in multicodecard

--- src/base_components/MultiCodeCard.js
+++ src/base_components/MultiCodeCard.js
@@ -4,7 +4,8 @@ import {
   Button,
   ButtonToolbar,
   ToggleButtonGroup,
-  ToggleButton
+  ToggleButton,
+  ListGroup
 } from "react-bootstrap";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { CodeHighlight, MultiCodeHighlight } from "./CodeHighlight";
@@ -17,79 +18,89 @@ const CodeCard = ({ cardTitle, codeSections }) => {
     <Card>
       <Card.Header>{cardTitle}</Card.Header>
       {codeSections.map(({ description, language, code, options, example }) => (
-        <div key={description}>
-          <Card.Body>
-            {description ? <Card.Text>{description}</Card.Text> : null}
-            <MultiCodeHighlight code={code} language={language} />
-          </Card.Body>
-          {options ? (
+        <ListGroup key={description}>
+          <ListGroup.Item>
             <Card.Body>
-              <Card.Title>Options</Card.Title>
-              <ButtonToolbar style={{ paddingBottom: "10px" }}>
-                <ToggleButtonGroup type="radio" name="options" defaultValue={2}>
-                  <ToggleButton
-                    variant="outline-secondary"
-                    value={1}
-                    onClick={() => setIsShowingOptions(true)}
-                  >
-                    Show
-                  </ToggleButton>
-                  <ToggleButton
-                    variant="outline-secondary"
-                    value={2}
-                    onClick={() => setIsShowingOptions(false)}
-                  >
-                    Hide
-                  </ToggleButton>
-                </ToggleButtonGroup>
-              </ButtonToolbar>
-              {isShowingOptions ? (
-                <>
-                  <CodeHighlight language={language}>{options}</CodeHighlight>
-                  {document.queryCommandSupported("copy") && (
-                    <CopyToClipboard text={code} onCopy={() => {}}>
-                      <Button variant="info">Copy</Button>
-                    </CopyToClipboard>
-                  )}
-                </>
-              ) : null}
+              {description ? <Card.Text>{description}</Card.Text> : null}
+              <MultiCodeHighlight code={code} language={language} />
             </Card.Body>
-          ) : null}
+            {options ? (
+              <Card.Body>
+                <Card.Title>Options</Card.Title>
+                <ButtonToolbar style={{ paddingBottom: "10px" }}>
+                  <ToggleButtonGroup
+                    type="radio"
+                    name="options"
+                    defaultValue={2}
+                  >
+                    <ToggleButton
+                      variant="outline-secondary"
+                      value={1}
+                      onClick={() => setIsShowingOptions(true)}
+                    >
+                      Show
+                    </ToggleButton>
+                    <ToggleButton
+                      variant="outline-secondary"
+                      value={2}
+                      onClick={() => setIsShowingOptions(false)}
+                    >
+                      Hide
+                    </ToggleButton>
+                  </ToggleButtonGroup>
+                </ButtonToolbar>
+                {isShowingOptions ? (
+                  <>
+                    <CodeHighlight language={language}>{options}</CodeHighlight>
+                    {document.queryCommandSupported("copy") && (
+                      <CopyToClipboard text={code} onCopy={() => {}}>
+                        <Button variant="info">Copy</Button>
+                      </CopyToClipboard>
+                    )}
+                  </>
+                ) : null}
+              </Card.Body>
+            ) : null}
 
-          {example ? (
-            <Card.Body>
-              <Card.Title>Example</Card.Title>
-              <ButtonToolbar style={{ paddingBottom: "10px" }}>
-                <ToggleButtonGroup type="radio" name="example" defaultValue={2}>
-                  <ToggleButton
-                    variant="outline-secondary"
-                    value={1}
-                    onClick={() => setIsShowingExample(true)}
+            {example ? (
+              <Card.Body>
+                <Card.Title>Example</Card.Title>
+                <ButtonToolbar style={{ paddingBottom: "10px" }}>
+                  <ToggleButtonGroup
+                    type="radio"
+                    name="example"
+                    defaultValue={2}
                   >
-                    Show
-                  </ToggleButton>
-                  <ToggleButton
-                    variant="outline-secondary"
-                    value={2}
-                    onClick={() => setIsShowingExample(false)}
-                  >
-                    Hide
-                  </ToggleButton>
-                </ToggleButtonGroup>
-              </ButtonToolbar>
-              {isShowingExample ? (
-                <>
-                  <CodeHighlight language={language}>{example}</CodeHighlight>
-                  {document.queryCommandSupported("copy") && (
-                    <CopyToClipboard text={code} onCopy={() => {}}>
-                      <Button variant="info">Copy</Button>
-                    </CopyToClipboard>
-                  )}
-                </>
-              ) : null}
-            </Card.Body>
-          ) : null}
-        </div>
+                    <ToggleButton
+                      variant="outline-secondary"
+                      value={1}
+                      onClick={() => setIsShowingExample(true)}
+                    >
+                      Show
+                    </ToggleButton>
+                    <ToggleButton
+                      variant="outline-secondary"
+                      value={2}
+                      onClick={() => setIsShowingExample(false)}
+                    >
+                      Hide
+                    </ToggleButton>
+                  </ToggleButtonGroup>
+                </ButtonToolbar>
+                {isShowingExample ? (
+                  <>
+                    <CodeHighlight language={language}>{example}</CodeHighlight>
+                    {document.queryCommandSupported("copy") && (
+                      <CopyToClipboard text={code} onCopy={() => {}}>
+                        <Button variant="info">Copy</Button>
+                      </CopyToClipboard>
+                    )}
+                  </>
+                ) : null}
+              </Card.Body>
+            ) : null}
+          </ListGroup.Item>
+        </ListGroup>
       ))}
     </Card>
   );
